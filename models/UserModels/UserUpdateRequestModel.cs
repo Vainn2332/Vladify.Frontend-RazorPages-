7229207175e15f377dbf6362966a8a541952e2e2
Vladislav Pomozov <vladuk2332@gmail.com>
2026-05-06 23:15:49 +0300

    add validation annotations

--- models/UserModels/UserUpdateRequestModel.cs
+++ models/UserModels/UserUpdateRequestModel.cs
@@ -1,4 +1,6 @@
-﻿namespace Vladify.Frontend.models.UserModels;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Vladify.Frontend.models.UserModels;
 
 public class UserUpdateRequestModel
 {
@@ -6,5 +8,6 @@ public class UserUpdateRequestModel
 
     public required int Age { get; set; }
 
+    [Range(1, 2, ErrorMessage = "Выберите пол")]
     public required Gender Gender { get; set; }
 }
