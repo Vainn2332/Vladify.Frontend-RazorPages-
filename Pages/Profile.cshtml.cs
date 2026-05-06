0b118e4f127ce108cb294c68c34fa919035bf23e
Vladislav Pomozov <vladuk2332@gmail.com>
2026-05-06 23:16:01 +0300

    edit Profile

--- Pages/Profile.cshtml.cs
+++ Pages/Profile.cshtml.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using Vladify.Frontend.models;
+using Vladify.Frontend.models.UserModels;
 using Vladify.Frontend.services;
 
 namespace Vladify.Frontend.Pages
@@ -16,13 +17,29 @@ namespace Vladify.Frontend.Pages
             _httpClient = httpClient;
         }
 
+        [BindProperty]
+        public UserUpdateRequestModel UserUpdateRequestModel { get; set; }
         public UserModel UserModel { get; set; }
 
-        public async Task OnGetAsync()
+
+        public async Task OnGetAsync(CancellationToken cancellationToken)
         {
             var accessToken = await HttpContext.GetTokenAsync("access_token");
 
-            UserModel = await _userService.GetCurrentUserAsync(accessToken);
+            UserModel = await _userService.GetCurrentUserAsync(accessToken, cancellationToken);
+        }
+
+        public async Task<IActionResult> OnPostAsync(CancellationToken cancellationToken)
+        {
+            if (!ModelState.IsValid)
+            {
+                return RedirectToPage();
+            }
+
+            var accessToken = await HttpContext.GetTokenAsync("access_token");
+            UserModel = await _userService.UpdateUserAsync(UserModel.Id, UserUpdateRequestModel, accessToken, cancellationToken);
+
+            return Page();
         }
     }
 }
