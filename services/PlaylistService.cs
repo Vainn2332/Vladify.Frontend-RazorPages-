64c7e9f87df24c99adbb2a09fd895403952a1318
Vladislav Pomozov <vladuk2332@gmail.com>
2026-05-07 23:55:48 +0300

    update playlistService

--- services/PlaylistService.cs
+++ services/PlaylistService.cs
@@ -5,7 +5,7 @@ namespace Vladify.Frontend.services;
 
 public class PlaylistService(HttpClient client)
 {
-    public async Task<IEnumerable<PlaylistModel>> GetPlaylistsOfUserAsync(Guid userId, PaginationFilter paginationFilter, string accessToken, CancellationToken cancellationToken)
+    public async Task<IEnumerable<PlaylistModel>> GetPlaylistsOfCurrentUserAsync(PaginationFilter paginationFilter, string accessToken, CancellationToken cancellationToken)
     {
         client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", accessToken);
         var playlists = await client.GetFromJsonAsync<IEnumerable<PlaylistModel>>($"{MyConstants.BaseApiUrl}/api/playlists?pageNumber={paginationFilter.PageNumber}&pageSize={paginationFilter.PageSize}", cancellationToken);
