a750cfa489805c5c54aa38edd5fdbf6dbb15cf76
Vladislav Pomozov <vladuk2332@gmail.com>
2026-05-08 18:28:07 +0300

    rename page+edit

--- Pages/Playlists.cshtml.cs
+++ Pages/Playlists.cshtml.cs
@@ -5,14 +5,14 @@ using Vladify.Frontend.services;
 
 namespace Vladify.Frontend.Pages;
 
-public class PlaylistDetailsModel(PlaylistService _playlistService) : PageModel
+public class PlaylistsModel(PlaylistService _playlistService) : PageModel
 {
     public PlaylistModel PlaylistModel { get; set; }
 
-    public async Task OnGetAsync(Guid playlistId, CancellationToken cancellationToken)
+    public async Task OnGetAsync(Guid id, CancellationToken cancellationToken)
     {
         var accessToken = await HttpContext.GetTokenAsync("access_token");
 
-        PlaylistModel = await _playlistService.GetPlaylistByIdAsync(playlistId, accessToken, cancellationToken);
+        PlaylistModel = await _playlistService.GetPlaylistByIdAsync(id, accessToken, cancellationToken);
     }
 }
