55210a251252a849b90f58d917752027a95bca30
Vladislav Pomozov <vladuk2332@gmail.com>
2026-05-09 21:15:54 +0300

    add update in service

--- services/PlaylistService.cs
+++ services/PlaylistService.cs
@@ -40,6 +40,24 @@ public class PlaylistService(HttpClient client)
         }
     }
 
+    public async Task<PlaylistModel> UpdatePlaylistAsync(PlaylistUpdateRequestModel playlistUpdateRequestModel, Guid playlistId, string accessToken, CancellationToken cancellationToken)
+    {
+        client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", accessToken);
+        var response = await client.PutAsJsonAsync($"{MyConstants.BaseApiUrl}/api/playlists/{playlistId}", playlistUpdateRequestModel, cancellationToken);
+
+        if (response.IsSuccessStatusCode)
+        {
+            var newPlaylist = await response.Content.ReadFromJsonAsync<PlaylistModel>();
+            return newPlaylist!;
+        }
+        else
+        {
+            var error = await response.Content.ReadFromJsonAsync<ErrorDetails>();
+            throw new Exception($"{error?.ErrorTitle}\n{error?.ErrorMessage}");
+        }
+    }
+
+
     public async Task<PlaylistModel> DeleteSongFromPlaylistAsync(Guid playlistId, Guid songId, string accessToken, CancellationToken cancellationToken)
     {
         client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", accessToken);
