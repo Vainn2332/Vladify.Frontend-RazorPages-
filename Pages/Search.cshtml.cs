7b39aa4bbd80c49717dba6bed25e7245ff64bd2d
Vladislav Pomozov <vladuk2332@gmail.com>
2026-05-10 19:50:11 +0300

    add search page

---
+++ Pages/Search.cshtml.cs
@@ -0,0 +1,19 @@
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Vladify.Frontend.models;
+using Vladify.Frontend.services;
+
+namespace Vladify.Frontend.Pages
+{
+    public class SearchModel(SearchService searchService) : PageModel
+    {
+        public async Task<SearchResultDto> OnGetAsync(string q, CancellationToken cancellationToken)
+        {
+            var accessToken = await HttpContext.GetTokenAsync("access_token");
+            // Твой SearchService вернет SearchResultDto
+            var result = await searchService.SearchAsync(q, accessToken!, cancellationToken);
+
+            return result;
+        }
+    }
+}
