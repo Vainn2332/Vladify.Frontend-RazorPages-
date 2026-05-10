1c135f6344786ca0a556743731eb4aea450844a4
Vladislav Pomozov <vladuk2332@gmail.com>
2026-05-10 19:36:33 +0300

    add service

---
+++ services/SearchService.cs
@@ -0,0 +1,15 @@
+﻿using System.Net.Http.Headers;
+using Vladify.Frontend.models;
+
+namespace Vladify.Frontend.services;
+
+public class SearchService(HttpClient client)
+{
+    public async Task<SearchResultDto> SearchAsync(string query, string accessToken, CancellationToken cancellationToken)
+    {
+        client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", accessToken);
+        var result = await client.GetFromJsonAsync<SearchResultDto>($"{MyConstants.BaseApiUrl}/api/search?query={query}", cancellationToken);
+
+        return result!;
+    }
+}
