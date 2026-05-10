b9fc84c7446bbc711643d4580ecff62a28bba61b
Vladislav Pomozov <vladuk2332@gmail.com>
2026-05-10 19:07:13 +0300

    add search dto

---
+++ models/SearchResultDto.cs
@@ -0,0 +1,9 @@
+﻿using Vladify.Frontend.models.UserModels;
+
+namespace Vladify.Frontend.models;
+
+public class SearchResultDto
+{
+    public ICollection<SongModel> Songs { get; set; }
+    public ICollection<UserModel> Users { get; set; }
+}
