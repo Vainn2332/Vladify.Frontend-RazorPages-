2c4f855a93729d929dbf98613511771dc542bc2e
Vladislav Pomozov <vladuk2332@gmail.com>
2026-05-10 12:10:40 +0300

    add add model

---
+++ models/SongModels/SongAddRequestModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Vladify.Frontend.models.SongModels;
+
+public class SongAddRequestModel
+{
+    public required string Title { get; set; }
+
+    public required string Album { get; set; }
+
+    public TimeSpan Duration { get; set; }
+}
