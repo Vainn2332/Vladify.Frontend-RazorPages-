0bad8593b36d41c622335f220011ac561da58bb8
Vladislav Pomozov <vladuk2332@gmail.com>
2026-05-10 20:54:53 +0300

    add constants

--- MyConstants.cs
+++ MyConstants.cs
@@ -5,4 +5,6 @@ public static class MyConstants
     public const string BaseApiUrl = "https://localhost:7161";
 
     public const int PlaylistsPaginationPageSize = 10;
+
+    public const int DropBoxPlaylistsPaginationPageSize = 100;
 }
