139bd9d66cfd0aa3c73ae9be312391e04421cc3f
Vladislav Pomozov <vladuk2332@gmail.com>
2026-05-10 12:10:48 +0300

    update service

--- services/SongService.cs
+++ services/SongService.cs
@@ -1,10 +1,28 @@
 ﻿using System.Net.Http.Headers;
 using Vladify.Frontend.models;
+using Vladify.Frontend.models.SongModels;
 
 namespace Vladify.Frontend.services;
 
 public class SongService(HttpClient client)
 {
+    public async Task<SongModel> CreateSongAsync(SongAddRequestModel songAddRequestModel, string accessToken, CancellationToken cancellationToken)
+    {
+        client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", accessToken);
+        var response = await client.PostAsJsonAsync<SongAddRequestModel>($"{MyConstants.BaseApiUrl}/api/songs", songAddRequestModel);
+
+        if (response.IsSuccessStatusCode)
+        {
+            var newSong = await response.Content.ReadFromJsonAsync<SongModel>();
+            return newSong!;
+        }
+        else
+        {
+            var error = await response.Content.ReadFromJsonAsync<ErrorDetails>();
+            throw new Exception($"{error?.ErrorTitle}\n{error?.ErrorMessage}");
+        }
+    }
+
     public async Task<ICollection<SongModel>> GetAllSongsOfUserAsync(Guid userId, string accessToken, CancellationToken cancellationToken)
     {
         client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", accessToken);
